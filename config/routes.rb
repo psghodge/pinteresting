fa83daa38a539958a303109247d4e3a78bdee831
Paresh Ghodge <psghodge@gmail.com>
2014-02-01 14:13:36 -0600

    created home & about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinteresting::Application.routes.draw do
+  root 'pages#home'
+  get 'about' =>'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,10 +13,7 @@ Pinteresting::Application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
+  # Example resource route (maps HTTP ons:
   #   resources :products do
   #     member do
   #       get 'short'
